87ce5948d352d020b308ed754aeb9794898b8be8
saadhaniftaj <your.email@example.com>
2025-08-17 05:24:03 +0500

    Change Nova voice from male (matthew) to female (sarah)

--- server.py
+++ server.py
@@ -88,7 +88,7 @@ class NovaSession:
                             "sampleRateHertz": 24000,
                             "sampleSizeBits": 16,
                             "channelCount": 1,
-                            "voiceId": "matthew",
+                            "voiceId": "sarah",
                             "encoding": "base64",
                             "audioType": "SPEECH",
                         },
